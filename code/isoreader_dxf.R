32e542ed059576b5a7ed5f6c46d28b20dc33e3ae
eqkuiper <esmeekuiper2028@u.northwestern.edu>
2025-10-14 16:32:55 -0500

    script to read in files

---
+++ code/isoreader_dxf.R
@@ -0,0 +1,10 @@
+# install and load required libraries
+if(!requireNamespace("devtools", quietly = TRUE)) install.packages("devtools")
+devtools::install_github("isoverse/isoreader")
+
+library("isoreader")
+
+# read in .dxf files
+dxf_raw <- iso_read_continuous_flow(snakemake@input[[dxf_raw]])
+
+iso_save(dxf_raw, snakemake@output[[out_file]])
