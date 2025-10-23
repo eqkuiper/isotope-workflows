795c653d2ef5e8998fbd8ab06fbfa8ffe1c2a3c3
eqkuiper <esmeekuiper2028@u.northwestern.edu>
2025-10-23 11:06:08 -0500

    add isoviewer

--- code/analysis_dxf.R
+++ code/analysis_dxf.R
@@ -1,5 +1,6 @@
 if(!requireNamespace("isoprocessor", quietly = TRUE)) devtools::install_github("isoverse/isoprocessor")
 
+# load required packages
 library("isoreader")
 library("isoprocessor")
 library("tidyverse")
@@ -10,12 +11,16 @@ dxf_comp <- dxf_raw %>%
   iso_filter_files_with_problems() # remove aborted runs, etc
 
 dxf_comp %>% 
-  iso_plot_continuous_flow_data() + 
-  scale_color_viridis_d(option="plasma")
+  iso_plot_continuous_flow_data() + # plot data
+  scale_color_viridis_d(option="plasma") # change chromatogram colors
 
-ggsave(paste0(Sys.Date(), "_all_chromatograms.png"),
+
+ggsave(paste0(Sys.Date(), "_all_chromatograms.png"), # save chromatogram plot
   width = 60,
   height = 40,
   limitsize=FALSE)
 
+dxf_ratios <- dxf_comp %>% 
+  iso_calculate_ratios(ratios = c("29/28", "30/28", "45/44", "46/44"))
 
+isoviewer::iso_start_viewer()
